0b097f09e598d712c445086d551025a51f8278b3
Rifky Alfarez <207006052@student.unsil.ac.id>
2024-08-22 22:31:06 +0700

    how it work section

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,9 +1,11 @@
 import HeroSection from '@/components/Home/HeroSection';
+import HowItWorksSection from '@/components/Home/HowItWorksSection';
 
 export default function HomePage() {
   return (
     <div>
       <HeroSection />
+      <HowItWorksSection />
     </div>
   );
 }
---
+++ src/components/Home/HowItWorksSection.tsx
@@ -0,0 +1,53 @@
+export default function HowItWorksSection() {
+  return (
+    <div className="bg-neutral-900 flex items-center justify-center py-14 px-8">
+      <div>
+        <div className="text-center">
+          <p className="text-sm font-bold uppercase text-neutral-500">
+            Cara Kerja
+          </p>
+          <h1 className="text-3xl font-bold text-neutral-50">
+            Sewa Kendaraan dengan 3 Langkah Mudah
+          </h1>
+        </div>
+        <div className="flex items-center justify-center">
+          <div>
+            <div></div>
+            <h1>Pilih Lokasi</h1>
+            <p>
+              Tentukan lokasi penjemputan terdekat yang paling nyaman bagi Anda.
+            </p>
+          </div>
+          <div>
+            <h1>Tentukan Tanggal</h1>
+            <p>Pilih tanggal dan waktu sewa sesuai dengan jadwal Anda.</p>
+          </div>
+          <div>
+            <h1>Booking Kendaraan</h1>
+            <p>Pesan kendaraan yang Anda inginkan dengan cepat dan mudah.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+{
+  /* <div className="bg-neutral-900">
+      <div className="px-8 flex items-center justify-center py-14">
+        <div className="flex flex-col items-center">
+          <span className="text-sm uppercase font-bold text-neutral-500">
+            Cara Kerja
+          </span>
+          <h1 className="text-3xl font-bold text-neutral-50">
+            Sewa Kendaraan Dengan 3 Langkah Mudah
+          </h1>
+          <div>
+            <div>Pilih Lokasi</div>
+            <div>Tentukan Tanggal</div>
+            <div>Booking Kendaraan</div>
+          </div>
+        </div>
+      </div>
+    </div> */
+}
